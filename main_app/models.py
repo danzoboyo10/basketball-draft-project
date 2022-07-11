62fc80a7a70c2b8a5a72f0cbef42c0a29d36a85a
Daniel Slebodnick <DanSlebodnick@Garys-MBP.cable.rcn.com>
2022-07-11 12:13:44 -0400

    wip: rendered index page and hardcoded list of bball players

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Player:
+  def __init__(self, name, age, college, ppg):
+    self.name = name
+    self.age = age
+    self.college = college
+    self.ppg = ppg
+
+players = [
+  Player('Tim', '23', 'ohio state', '30.9'),
+  Player('Jim', '24', 'ball state', '20.9'),
+  Player('Larry', '26', 'missouri state', '22.0'),
+  Player('Gary', '29', 'oklahoma state', '24'),
+]
--- main_app/urls.py
+++ main_app/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path
 from . import views
+
 urlpatterns = [
-    path('', views.home)
+    path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('players/', views.players_index, name='index'),
+
+    # path('players', views.index, name='index'),
+    
 ]
